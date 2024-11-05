250008da4103e1247041fd895ef78e7868bc4bc2
Victor Suetnov <v.suetnov@citrus-soft.ru>
2024-11-05 15:26:32 +0300

    execute ex2-34

---
+++ local/templates/furniture_blue/components/bitrix/news/.default/bitrix/news.list/.default/result_modifier.php
@@ -0,0 +1,6 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$arResult['SPECIALDATE'] = $arResult['ITEMS'][0]["ACTIVE_FROM"];
+
+$this->__component->SetResultCacheKeys(['SPECIALDATE']);
