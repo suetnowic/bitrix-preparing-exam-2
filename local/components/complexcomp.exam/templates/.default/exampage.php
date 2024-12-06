099824ee8f2991472e2a9ead4ca6819bf8b5e33c
Victor Suetnov <v.suetnov@citrus-soft.ru>
2024-12-06 14:52:51 +0300

    добавлен пропущенный символ =

--- local/components/complexcomp.exam/templates/.default/exampage.php
+++ local/components/complexcomp.exam/templates/.default/exampage.php
@@ -1,4 +1,4 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
-<p><? GetMessage("EXAM_COMP_PARAM1", ["#PARAM1#" => $arResult['VARIABLES']['PARAM1']]); ?></p>
-<p><? GetMessage("EXAM_COMP_PARAM2", ["#PARAM2#" => $arResult['VARIABLES']['PARAM2']]); ?></p>
+<p><?= GetMessage("EXAM_COMP_PARAM1", ["#PARAM1#" => $arResult['VARIABLES']['PARAM1']]); ?></p>
+<p><?= GetMessage("EXAM_COMP_PARAM2", ["#PARAM2#" => $arResult['VARIABLES']['PARAM2']]); ?></p>
