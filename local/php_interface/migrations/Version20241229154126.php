984272c9556d88e2cbe43b9331dbdba7da8d40b9
suetnowic <mr-zjops19882007@yandex.ru>
2024-12-29 15:45:07 +0300

    ex2-108 добавлены миграции

---
+++ local/php_interface/migrations/Version20241229154126.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Sprint\Migration;
+
+class Version20241229154126 extends Version
+{
+    protected $author = "admin";
+
+    protected $description   = "Элементы инфоблока Canonical";
+
+    protected $moduleVersion = "4.16.1";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->IblockElementsImport()
+             ->setExchangeResource('iblock_elements.xml')
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Iblock()
+                      ->addElement(
+                          $item['iblock_id'],
+                          $item['fields'],
+                          $item['properties']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function down()
+    {
+    }
+}
