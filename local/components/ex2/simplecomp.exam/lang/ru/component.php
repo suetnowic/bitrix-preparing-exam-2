3f8adc0cf345d8d09931115a645eb7e0197423da
suetnowic <mr-zjops19882007@yandex.ru>
2024-12-30 11:42:52 +0300

    exec ex2-100

--- local/components/ex2/simplecomp.exam/lang/ru/component.php
+++ local/components/ex2/simplecomp.exam/lang/ru/component.php
@@ -1,4 +1,5 @@
 <?
 $MESS["SIMPLECOMP_EXAM2_IBLOCK_MODULE_NONE"] = "Модуль Информационных блоков не установлен";
 $MESS["COUNT_NEWS"] = "Выбранных новостей - #COUNT#";
+$MESS["IB_ADMIN"] = "ИБ в админке";
 ?>
