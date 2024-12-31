958b515a808e932bf9594a9b6c3969d9c8af53f9
suetnowic <mr-zjops19882007@yandex.ru>
2024-12-31 15:39:15 +0300

    exec ex2-95 all-type-ticket

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -3,3 +3,4 @@
 include __DIR__ . "/events/deactivation_product.php";
 include __DIR__ . "/events/write_event_log.php";
 include __DIR__ . "/events/feedback.php";
+include __DIR__ . "/events/change_global_menu.php";
