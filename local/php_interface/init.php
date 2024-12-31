225487346a3e8b7f04436f8d6e001307bc9355d3
suetnowic <mr-zjops19882007@yandex.ru>
2024-12-31 14:52:51 +0300

    exec ex2-51

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,4 +1,5 @@
-<?
+<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 
 include __DIR__ . "/events/deactivation_product.php";
 include __DIR__ . "/events/write_event_log.php";
+include __DIR__ . "/events/feedback.php";
