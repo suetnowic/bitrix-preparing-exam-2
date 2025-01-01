fb25c77b23e0de6661d471377bbd2da3688c58a9
suetnowic <mr-zjops19882007@yandex.ru>
2025-01-01 11:02:38 +0300

    ex2-70 переименован каталог компонента

--- local/components/simplecomp.exam_ex2-70/.description.php
+++ local/components/simplecomp.exam_ex2-70/.description.php
@@ -4,7 +4,8 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 $arComponentDescription = array(
 	"NAME" => GetMessage("SIMPLECOMP_EXAM2_NAME"),
 	"PATH" => array(
-		"ID" => "ex2simple",
+		"ID" => "ex2",
+		"NAME" => GetMessage("SECTION"),
 	),
 );
 ?>
