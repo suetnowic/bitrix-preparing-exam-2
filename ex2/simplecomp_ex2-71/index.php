be6b2b1113ca86331a3fb1a02b776f9529160cae
suetnowic <mr-zjops19882007@yandex.ru>
2025-01-02 17:00:03 +0300

    fix

--- ex2/simplecomp_ex2-71/index.php
+++ ex2/simplecomp_ex2-71/index.php
@@ -1,8 +1,18 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Простой компонент");
-?>
-
-Text here....
-
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?><?$APPLICATION->IncludeComponent(
+	"simplecomp.exam_ex2-71", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "36000000",
+		"CACHE_GROUPS" => "Y",
+		"PRODUCTS_IBLOCK_ID" => "2",
+		"CLASSIFICATOR_IBLOCK_ID" => "7",
+		"TEMPLATE_DETAIL_URL" => "/products/#SECTION_ID#/#ELEMENT_ID#/",
+		"PROPERTY_CODE" => "COMPANY"
+	),
+	false
+);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
