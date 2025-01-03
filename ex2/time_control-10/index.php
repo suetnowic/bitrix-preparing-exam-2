7565240807e309b99219572c5b6217fa1e76ba28
suetnowic <mr-zjops19882007@yandex.ru>
2025-01-03 17:04:00 +0300

    exec ex2-10

---
+++ ex2/time_control-10/index.php
@@ -0,0 +1,10 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности 10");
+?>
+
+Самая ресурсоемкая страница: /products/index.php и ее доля % в общей статистике нагрузки: 22.17% среднее время генерации: 5.0241
+<br>
+компонент: bitrix:catalog и время его работы: 0.0376 с
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
