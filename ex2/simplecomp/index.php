d238183d643d16a8df1457ec9a680227a0b7849c
suetnowic <mr-zjops19882007@yandex.ru>
2025-01-03 13:24:29 +0300

    exec ex2-60

--- ex2/simplecomp/index.php
+++ ex2/simplecomp/index.php
@@ -2,14 +2,17 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Простой компонент");
 ?><?$APPLICATION->IncludeComponent(
-	"simplecomp.exam_ex2-70",
-	"",
-	Array(
+	"simplecomp.exam_ex2-70", 
+	".default", 
+	array(
 		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "A",
 		"NEWS_IBLOCK_ID" => "1",
 		"PRODUCTS_IBLOCK_ID" => "2",
 		"TEMPLATE_DETAIL_URL" => "catalog_exam/#SECTION_ID#/#ELEMENT_CODE#",
-		"UF_PROP_CODE" => "UF_NEWS_LINK"
-	)
+		"UF_PROP_CODE" => "UF_NEWS_LINK",
+		"COMPONENT_TEMPLATE" => ".default",
+		"ELEMENT_PER_PAGE" => "2"
+	),
+	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- local/components/simplecomp.exam_ex2-70/lang/ru/component.php
+++ local/components/simplecomp.exam_ex2-70/lang/ru/component.php
@@ -2,4 +2,5 @@
 $MESS["SIMPLECOMP_EXAM2_IBLOCK_MODULE_NONE"] = "Модуль Информационных блоков не установлен";
 $MESS["SIMPLECOMP_EXAM2_TITLE"] = "В каталоге товаров представлено товаров: #COUNT#";
 $MESS["SUBMENU_IB_ADMIN"] = "ИБ в админке";
+$MESS["PAGE_TITLE"] = "Страницы";
 ?>
